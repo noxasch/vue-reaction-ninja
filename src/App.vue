cb2af75c59617d7a4c76b2ece33c01663d2aa781
Alexander Dischberg <noxasch@gmail.com>
2024-01-19 02:12:02 +0800

    feat: add block component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <script setup>
+  import { ref } from 'vue';
+  import Block from './components/Block.vue';
+
+  const isPlaying = ref(false);
+  const delay = ref(null);
+
+  function start() {
+    delay.value = 2000 + Math.random() * 5000;
+    isPlaying.value = true;
+    console.log(delay.value);
+  }
 </script>
 
 <template>
-  <h1>A Vue3 Project</h1>
+  <h1>Reaction Ninja</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <style scoped>
---
+++ src/components/Block.vue
@@ -0,0 +1,29 @@
+<script setup>
+  import { ref, onMounted } from 'vue';
+
+  const showBlock = ref(false);
+  const props = defineProps(['delay']);
+
+  onMounted(() => {
+    setTimeout(() => {
+      showBlock.value = true;
+      console.log(props.delay);
+    }, props.delay);
+  });
+</script>
+
+<template>
+  <div class="block" v-if="showBlock"></div>
+</template>
+
+<style scoped>
+  .block {
+    width: 400px;
+    border-radius: 20px;
+    background: #0faf87;
+    color: #fff;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+  }
+</style>
